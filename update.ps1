6c0fecfba0488d964251d3b32fcbfe7e5e0eba3e
cwhiterite <cwhiterite@gmail.com>
2021-08-31 16:20:09 -0600

    created html docs

--- update.ps1
+++ update.ps1
@@ -1,7 +1,8 @@
 
 # IMPORT ARRAYLIST - User Objects from CSV
 # User Object has Name, Desk, X, Y properties
-[System.Collections.ArrayList]$users = Import-CSV "C:\temp\users.csv"
+[System.Collections.ArrayList]$users = Import-CSV ".\users.csv"
+$users = $users | Sort-Object -Property Name;
 
 # EXPORT ARRAYLIST - User Objects to CSV
 # User Object has Name, Desk, X, Y properties
@@ -23,6 +24,10 @@ function Remove-User {
     if ($ArrayList[$i].Name -eq $UserName) {
       $ArrayList.RemoveAt($i);
     }
+    else {
+      Write-Host "$UserName is not on the map!"
+
+    }
   }
   $ArrayList = $ArrayList | Sort-Object -Property Name;
   return $ArrayList;
@@ -41,12 +46,27 @@ function Add-User {
 
   $ArrayList = $UserList.Clone();
 
-  $user = [PSCustomObject]@{
-    Name = $UserName; Desk = $UserDesk; X = "0"; Y = "0"
-  };
-  $ArrayList.Add($user) | Out-Null;
-  $ArrayList = $ArrayList | Sort-Object -Property Name;
-  return $ArrayList;
+  $ArrayList | ForEach-Object {
+    if ($UserDesk -eq $_.Desk) {
+      $desk = $_.Desk;
+      if ($_.X -eq "" -or $_.Y -eq "") {
+        $desk = $_.Desk;
+        Write-Host "Coords not found for desk: $desk!"
+      }
+      else {
+        if ($_.Name -eq "") {
+          $user = [PSCustomObject]@{Name = $UserName; Desk = $UserDesk; };
+          $ArrayList.Add($user) | Out-Null;
+          $ArrayList = $ArrayList | Sort-Object -Property Name;
+          return $ArrayList;
+        }
+        else {
+          $name = $_.Name;
+          Write-Host "Desk: $desk is currently occupied by $name!"
+        }
+      }
+    }
+  }
 }
 
 function Add-Spaces {
@@ -70,7 +90,7 @@ function Format-JS {
 
 # Get static js code and then insert users
 $static = Get-Content .\static.js;
-$snip1 = $static[0..1];
+$snip1 = $static[0..17];
 $snip3 = $static[19..20];
 $snip2 = Format-JS -UserList $users
 $snippet = $snip1 + $snip2 + $snip3;
@@ -80,32 +100,65 @@ $snippet | out-file .\output.js
 
 
 # Find Coords for Desk#
-$users | ForEach-Object {
-  if ($_.Desk -eq "") { $_.X = ""; $_.Y = ""; }
-  elseif ($_.Desk -eq "") { $_.X = ""; $_.Y = ""; }
-  else { $_.X = ""; $_.Y = ""; }
+# $users | ForEach-Object {
+#   if ($_.Desk -eq "") { $_.X = ""; $_.Y = ""; }
+#   elseif ($_.Desk -eq "") { $_.X = ""; $_.Y = ""; }
+#   else { $_.X = ""; $_.Y = ""; }
+# }
+
+
+function Get-Alphabet {
+  $chars = @();
+  65..90 | ForEach-Object { $chars += [char]$_ };
+
+  $alpha = @();
+  for ($i = 0; $i -lt 26; $i++) {
+    $alpha += [string]$chars[$i];
+  }
+  return $alpha;
 }
-
-
 
 # Format HTML code
+$alpha = Get-Alphabet;
+$ac = $alpha[0..2];
+$dj = $alpha[3..9];
+$ko = $alpha[10..14];
+$pz = $alpha[15..26];
 
+$index = ".\index.html"
+$page2 = ".\page2.html"
+$page3 = ".\page3.html"
+$page4 = ".\page4.html"
+
+#takes a range of letters, formats names into html code, exports to file
 function Format-HTML {
-  param ([Parameter(Mandatory)]$UserList)
-  $string = @()
-  $UserList | ForEach-Object {
-    $name = $_.Name;
-    # Find Ranges of Names by First Initial
-    switch ($name) {
-      condition {}
+  param (
+    [Parameter(Mandatory)]$List,
+    [Parameter(Mandatory)]$Range,
+    [Parameter(Mandatory)]$File,
+    [Parameter(Mandatory)]$Date
+  )
+
+  $static = Get-Content .\static.html;
+  $snip1 = $static[0..16];
+  $snip2 = @();
+  $snip3 = $static[18..31];
+  $snip4 = (Add-Spaces 4) + "<div class=`"updated`">Updated: $Date</p>"
+  $snip5 = $static[33..35];
+
+  for ($i = 0; $i -lt $Range.count; $i++) {
+    $List | ForEach-Object {
+      $name = $_.Name;
+      $pattern = $Range[$i] + "*";
+      if ($name -like $pattern) {
+        $line = (Add-Spaces 10) + "<option>$name</option>";
+        $snip2 += $line;
+      }
     }
-    $line = (Add-Spaces 20) + "<option>$name</option>";
-    $string += $line;
+    $snippet = $snip1 + $snip2 + $snip3 + $snip4 + $snip5;
   }
-  return $string;
-
+  $snippet | out-file $File;
 }
-
 
 
 
@@ -125,6 +178,11 @@ function Set-Users {
   $user | export-csv c:\temp\users.csv -NoTypeInformation
   Write-Host $user;
 }
+
+Format-HTML -Range $ac -List $users -File $index -Date "8/31"
+Format-HTML -Range $dj -List $users -File $page2 -Date "8/31"
+Format-HTML -Range $ko -List $users -File $page3 -Date "8/31"
+Format-HTML -Range $pz -List $users -File $page4 -Date "8/31"
 
 #TODO :
 # DONE: Function that adds users from csv with name, desk, inserts into users ArrayList
